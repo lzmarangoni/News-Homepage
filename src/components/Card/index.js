637a6bff6ffcceeaa2b3631d2615b5306ceff06a
Luiz Marangoni <lzmarangoni@gmail.com>
2023-08-14 23:48:21 -0300

    construção de card simples com informações chumbadas apenas para teste

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import styles from './Card.module.css'
+import React from "react";
+import styles from "./Card.module.css";
 
-export default function Card({image, title, text}) {
+export default function Card() {
   return (
-    <div> 
-        <img src={image}/>
-        <h1>{title}</h1>
-        <p>{text}</p>
-
+    <div className={styles.card}>
+      <img src="../images/image-web-3-desktop.jpg" />
+      <div className={styles.cardDescription}>
+        <h1>The Bright Future of Web 3.0?</h1>
+        <div className={styles.cardText}>
+          <p>
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button className={styles.buttonCustom}>Ver Mais</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Card from '../../components/Card'
-import noticias from '../../news.json'
+
 
 export default function Home() {
   return (
     <div>
-      <button onClick={()=>{console.log(noticias)}}></button>
-      <Card />
+      <Card/>
+      
     </div>
     
    
